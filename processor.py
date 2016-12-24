9d376bb3378e472766841e37f1313a7198f66967
Ciaran <iCiaran@users.noreply.github.com>
2016-12-24 12:54:37 +0000

    Implemented bitwise and/or/not

--- processor.py
+++ processor.py
@@ -7,9 +7,11 @@ class Processor:
         self._maxMemory = 2**15
         self._get = {0: self.halt, 21: self.noop, 19: self.out, 6: self.jmp,
                      7: self.jt, 8: self.jf, 1: self.set, 9: self.add,
-                     4: self.eq, 2: self.push, 3: self.pop}
+                     4: self.eq, 2: self.push, 3: self.pop, 5: self.gt,
+                     12: self.bit_and, 13: self.bit_or, 14: self.bit_not}
         self._nArg = {0: 0, 21: 0, 19: 1, 6: 1, 7: 2, 8: 2, 1: 2, 9: 3,
-                      4: 3, 2: 1, 3: 1}
+                      4: 3, 2: 1, 3: 1, 5: 3, 12: 3, 13: 3, 14: 2}
+        # print("-".join([str(x) for x in self._get]))
         self._main = []
         self._registers = {}
         self._stack = []
@@ -28,6 +30,27 @@ class Processor:
         # self._registers[args[0] % self._maxMemory] = self.getValue(args[1])
         self._pc += 3
 
+    def bit_and(self, args):
+        self.writeToRegister(args[0], self.getValue(args[1]) &
+                             self.getValue(args[2]))
+        self._pc += 4
+
+    def bit_or(self, args):
+        self.writeToRegister(args[0], self.getValue(args[1]) |
+                             self.getValue(args[2]))
+        self._pc += 4
+
+    def bit_not(self, args):
+        self.writeToRegister(args[0], ~self.getValue(args[1]) & 0x7FFF)
+        self._pc += 3
+
+    def gt(self, args):
+        if self.getValue(args[1]) > self.getValue(args[2]):
+            self.writeToRegister(args[0], 1)
+        else:
+            self.writeToRegister(args[0], 0)
+        self._pc += 4
+
     def push(self, args):
         self._stack.append(self.getValue(args[0]))
         self._pc += 2
@@ -35,6 +58,8 @@ class Processor:
     def pop(self, args):
         if len(self._stack) > 0:
             self.writeToRegister(args[0], self._stack.pop())
+        else:
+            self.halt(None)
         self._pc += 2
 
     def add(self, args):
@@ -57,7 +82,7 @@ class Processor:
         self._pc += 1
 
     def out(self, args):
-        print(chr(args[0]), end="")
+        print(chr(self.getValue(args[0])), end="")
         self._pc += 2
 
     def jmp(self, args):
@@ -78,8 +103,7 @@ class Processor:
             self._pc += 3
 
     def wmem(self, args):
-        self._main[self.getValue(args[0])] = self.getValue(
-                                                                    args[1])
+        self._main[self.getValue(args[0])] = self.getValue(args[1])
         self._pc += 3
 
     def loadProgram(self, i, hword):
@@ -98,10 +122,10 @@ class Processor:
         if op not in self._get:
             print("{}: Opcode {} not implemented yet!".format(self._pc, op))
             self._pc += 1
-            sys.exit(0)
+            self.halt(None)
         n = self._nArg[op]
         a = [self._main[self._pc + i] for i in range(1, self._nArg[op] + 1)]
-        print(self._pc, self._get[op].__name__, n, a)
+        # print(self._pc, self._get[op].__name__, n, a)
         self._get[op](a)
 
     def getValue(self, a):
@@ -112,4 +136,4 @@ class Processor:
             return self._registers[a % self._maxMemory]
         else:
             print("Invalid value/register")
-            system.exit(0)
+            self.halt(None)
