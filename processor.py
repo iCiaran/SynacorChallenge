21c91432970fd0e8faa439a4b6c251c83750fb3f
Ciaran <iCiaran@users.noreply.github.com>
2016-12-24 11:25:34 +0000

    Implemented add/eq

--- processor.py
+++ processor.py
@@ -5,18 +5,40 @@ class Processor:
 
     def __init__(self):
         self._maxMemory = 2**15
-        self._get = {0: self.halt, 21: self.noop, 19: self.out, 6: self.jmp,         7: self.jt, 8: self.jf, 1: self.set}
-        self._nArg = {0: 0, 21: 0, 19: 1, 6: 1, 7: 2, 8:2, 1:2}
+        self._get = {0: self.halt, 21: self.noop, 19: self.out, 6: self.jmp,
+                     7: self.jt, 8: self.jf, 1: self.set, 9: self.add,
+                     4: self.eq}
+        self._nArg = {0: 0, 21: 0, 19: 1, 6: 1, 7: 2, 8: 2, 1: 2, 9: 3,
+                      4: 3}
         self._main = []
         self._registers = {}
         self._pc = 0
         self.initRegisters()
         self.initMemory()
 
+    def writeToRegister(self, reg, val):
+        if 32768 <= reg <= 32775:
+            self._registers[reg % self._maxMemory] = val
+        else:
+            print("Invalid register", reg, val)
+
     def set(self, args):
-        self._registers[args[0] % self._maxMemory] = self.getValue(args[1])
-        print(self._registers)
+        self.writeToRegister(args[0], args[1])
+        # self._registers[args[0] % self._maxMemory] = self.getValue(args[1])
         self._pc += 3
+
+    def add(self, args):
+        self.writeToRegister(args[0], (self.getValue(args[1]) +
+                                       self.getValue(args[2])) %
+                             self._maxMemory)
+        self._pc += 4
+
+    def eq(self, args):
+        if self.getValue(args[1]) == self.getValue(args[2]):
+            self.writeToRegister(args[0], 1)
+        else:
+            self.writeToRegister(args[0], 0)
+        self._pc += 4
 
     def halt(self, args):
         sys.exit(0)
@@ -29,11 +51,11 @@ class Processor:
         self._pc += 2
 
     def jmp(self, args):
-        #print("jmp,", args)
+        # print("jmp,", args)
         self._pc = self.getValue(args[0])
 
     def jt(self, args):
-        #print(self.getValue(args[0]))
+        # print(self.getValue(args[0]))
         if self.getValue(args[0]) != 0:
             self._pc = self.getValue(args[1])
         else:
@@ -62,24 +84,22 @@ class Processor:
 
     def doOperation(self):
         op = self._main[self._pc]
-        
+
         if op not in self._get:
             print("{}: Opcode {} not implemented yet!".format(self._pc, op))
             self._pc += 1
             sys.exit(0)
         n = self._nArg[op]
         a = [self._main[self._pc + i] for i in range(1, self._nArg[op] + 1)]
-        #print(self._pc, op, n, a)
+        print(self._pc, self._get[op].__name__, n, a)
         self._get[op](a)
 
     def getValue(self, a):
         if a < self._maxMemory:
             return a
         elif a < self._maxMemory + 8:
-            #print("Accessed Register", self._pc, a % self._maxMemory)
+            # print("Accessed Register", self._pc, a % self._maxMemory)
             return self._registers[a % self._maxMemory]
         else:
             print("Invalid value/register")
             system.exit(0)
-
-    
